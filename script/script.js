4a21ba76ed60f2a1c3afc823a86cc642f654cbd9
HelenSeverina <helen.severina23@gmail.com>
2023-03-09 23:44:47 +0200

    no commit message

--- script/script.js
+++ script/script.js
@@ -4,9 +4,3 @@ document.addEventListener('DOMContentLoaded', function() {
     document.querySelector('.burger-menu').classList.toggle('active');
   })
 });
-
-// document.addEventListener('DOMContentLoaded', function() {
-//   document.querySelector('.slider-items').addEventListener('click', function() {
-//     document.querySelector('.burger').classList.toggle('active');
-//   })
-// });
